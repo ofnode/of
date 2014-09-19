f71dc78b3f8e880f83ce0f9c9c04ce19669b0b67
Constantine Tarasenkov <iamvfx@gmail.com>
2014-09-19 23:53:56 +0400

    Do not open analyzer's report automatically

--- Template/analyze.sh
+++ Template/analyze.sh
@@ -12,5 +12,5 @@ cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Debug \
 -DCMAKE_CXX_COMPILER=$ANALYSE/c++-analyzer \
   -DCMAKE_C_COMPILER=$ANALYSE/ccc-analyzer
 
-scan-build -analyze-headers --use-cc=clang --use-c++=clang++ --view ninja
+scan-build -analyze-headers --use-cc=clang --use-c++=clang++ ninja
 
