31a766afac48557ac7d7864019060d45f4f7d627
Constantine Tarasenkov <iamvfx@gmail.com>
2014-10-18 04:37:02 +0400

    Fix for folders with spaces for diff.sh script

--- dev/tools/diff.sh
+++ dev/tools/diff.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-cd `dirname $(readlink -f $0)`
+cd "`dirname "$(readlink -f $0)"`"
 cd ../..
-OF=`pwd`
+OF="`pwd`"
 
-ORIGINS=$(find $OF/src -type f -name *.origin)
+ORIGINS=$(find "$OF/src" -type f -name *.origin)
 
-for i in $ORIGINS; do
-    DIR=$(basename `dirname $i`)
-    mkdir -p $OF/dev/tools/$DIR
-    diff -Naur $i `dirname $i`/`basename ${i%.*}` > $OF/dev/tools/$DIR/`basename ${i%.*}`.patch
+for i in "$ORIGINS"; do
+    DIR="`basename "$(dirname $i)"`"
+    mkdir -p "$OF"/dev/tools/"$DIR"
+    diff -Naur "$i" "`dirname "$i"`"/"`basename "${i%.*}"`" > "$OF"/dev/tools/"$DIR"/"`basename "${i%.*}"`".patch
 done
