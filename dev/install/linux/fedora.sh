c0b5e9a04491bfcc489e8071f3791f6ed746a2f7
Constantine <iamvfx@gmail.com>
2014-12-13 23:15:13 +0300

    Update fedora.sh

--- dev/install/linux/fedora.sh
+++ dev/install/linux/fedora.sh
@@ -6,43 +6,43 @@ http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedo
 http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 sudo yum -y update
-sudo yum -y install    \
-pkgconfig              \
-libstdc++-static       \
-xorg-x11-server-devel  \
-libXcursor-devel       \
-libXrandr-devel        \
-libXi-devel            \
-gtk3-devel             \
-libudev-devel          \
-llvm                   \
-clang                  \
-clang-analyzer         \
-cmake                  \
-ninja-build            \
-cairo-devel            \
-libusb-devel           \
-openssl-devel          \
-freetype-devel         \
-fontconfig-devel       \
-mesa-libGLU-devel      \
-openal-soft-devel      \
-opencv-devel           \
-tbb-devel              \
-mpg123                 \
-flac-devel             \
-alsa-lib-devel         \
-libtheora-devel        \
-libvorbis-devel        \
-libsndfile-devel       \
-pulseaudio-libs-devel  \
-ffmpeg-devel           \
-libmpg123-devel        \
-libraw1394-devel       \
-gstreamer-devel        \
-gstreamer-ffmpeg       \
-gstreamer-plugins-ugly \
-gstreamer-plugins-base-devel
+sudo yum -y install     \
+pkgconfig               \
+libstdc++-static        \
+xorg-x11-server-devel   \
+libXcursor-devel        \
+libXrandr-devel         \
+libXi-devel             \
+gtk3-devel              \
+libudev-devel           \
+llvm                    \
+clang                   \
+clang-analyzer          \
+cmake                   \
+ninja-build             \
+cairo-devel             \
+libusb-devel            \
+openssl-devel           \
+freetype-devel          \
+fontconfig-devel        \
+mesa-libGLU-devel       \
+openal-soft-devel       \
+opencv-devel            \
+tbb-devel               \
+mpg123                  \
+flac-devel              \
+alsa-lib-devel          \
+libtheora-devel         \
+libvorbis-devel         \
+libsndfile-devel        \
+pulseaudio-libs-devel   \
+ffmpeg-devel            \
+libmpg123-devel         \
+libraw1394-devel        \
+gstreamer1-devel        \
+gstreamer1-libav        \
+gstreamer1-plugins-ugly \
+gstreamer1-plugins-base-devel
 
 sudo ln -s /usr/bin/ninja-build /usr/bin/ninja 2> /dev/null
 
