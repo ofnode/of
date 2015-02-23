dd5da989861dc50928289eae435790c4661faf4e
Constantine Tarasenkov <constantine@protonmail.com>
2015-02-23 19:07:26 +0300

    Arch Linux install script update

--- dev/install/linux/archlinux.sh
+++ dev/install/linux/archlinux.sh
@@ -4,34 +4,36 @@
 yes | sudo pacman -Syu
 
 yes | sudo pacman -Sy --needed \
-git               \
-make              \
-pkg-config        \
-xorg-server-devel \
-libxcursor        \
-libxrandr         \
-libxi             \
-gtk3              \
-llvm              \
-clang             \
-clang-analyzer    \
-cmake             \
-ninja             \
-cairo             \
-libusb            \
-openssl           \
-freetype2         \
-fontconfig        \
-glu               \
-openal            \
-opencv            \
-intel-tbb         \
-mpg123            \
-libsndfile        \
-gst-libav         \
-gstreamer         \
-gst-plugins-base  \
-gst-plugins-good  \
-gst-plugins-bad   \
+git                \
+make               \
+pkg-config         \
+xorg-server        \
+xorg-server-devel  \
+xorg-server-utils  \
+xorg-server-common \
+libxcursor         \
+libxrandr          \
+libxi              \
+gtk3               \
+llvm               \
+clang              \
+clang-analyzer     \
+cmake              \
+ninja              \
+cairo              \
+libusb             \
+openssl            \
+freetype2          \
+fontconfig         \
+glu                \
+openal             \
+opencv             \
+intel-tbb          \
+mpg123             \
+libsndfile         \
+gst-libav          \
+gstreamer          \
+gst-plugins-base   \
+gst-plugins-good   \
+gst-plugins-bad    \
 gst-plugins-ugly
-
