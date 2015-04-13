77881c7e06c8dcdc878970a203db097d28bf6834
Constantine Tarasenkov <constantine@protonmail.com>
2015-04-13 15:26:45 +0300

    Split build script

---
+++ dev/build-linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd "`dirname "$(readlink -f $0)"`"
+cd ..
+OF="`pwd`"
+
+#-------------------------------------------------------------------------------
+
+mkdir -p "$OF/build/linux/release"
+cd       "$OF/build/linux/release"
+cmake    "$OF" -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release
+ninja
+
+mkdir -p "$OF/build/linux/debug"
+cd       "$OF/build/linux/debug"
+cmake    "$OF" -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug
+ninja
+
+cd  "$OF"
+tar -czvf lib-linux.tar.gz lib-linux
--- dev/build-mxe.sh
+++ dev/build-mxe.sh
@@ -5,18 +5,6 @@ OF="`pwd`"
 
 #-------------------------------------------------------------------------------
 
-mkdir -p "$OF/build/linux/release"
-cd       "$OF/build/linux/release"
-cmake    "$OF" -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release
-ninja
-
-mkdir -p "$OF/build/linux/debug"
-cd       "$OF/build/linux/debug"
-cmake    "$OF" -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug
-ninja
-
-if [ -d /opt/mxe ]; then
-
 mkdir -p "$OF/build/windows/release"
 cd       "$OF/build/windows/release"
 cmake    "$OF" -G Ninja -DCMAKE_TOOLCHAIN_FILE=`find /opt/mxe -name mxe-conf.cmake` -DCMAKE_BUILD_TYPE=Release
@@ -27,8 +15,5 @@ cd       "$OF/build/windows/debug"
 cmake    "$OF" -G Ninja -DCMAKE_TOOLCHAIN_FILE=`find /opt/mxe -name mxe-conf.cmake` -DCMAKE_BUILD_TYPE=Debug
 ninja
 
-fi
-
 cd  "$OF"
-tar -czvf lib-linux.tar.gz lib-linux
-tar -czvf lib-windows.tar.gz lib-windows
+tar -czvf lib-windows-mxe.tar.gz lib-windows
