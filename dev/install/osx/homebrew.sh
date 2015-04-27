3b4764db75cac52c19d68dd4aa10c69c431824ab
Felix <fdubrownik@gmail.com>
2015-04-27 13:09:24 +0200

    added some find_packages

--- dev/install/osx/homebrew.sh
+++ dev/install/osx/homebrew.sh
@@ -3,3 +3,6 @@
 brew update
 brew install Caskroom/cask/xquartz # needed for cairo
 brew install libsndfile cairo pkgconfig
+
+# cairo dependencies automatically installed:
+# libpng, freetype, fontconfig, pixman, gettext, libffi, glib
