5897445b92903caa5a6e6999af71c475d03f2620
Felix <fdubrownik@gmail.com>
2015-04-27 14:03:39 +0200

    added Freetype headers, added cmake to homebrew

--- dev/install/osx/homebrew.sh
+++ dev/install/osx/homebrew.sh
@@ -2,7 +2,7 @@
 
 brew update
 brew install Caskroom/cask/xquartz # needed for cairo
-brew install libsndfile cairo pkgconfig
+brew install libsndfile cairo pkgconfig cmake
 
 # cairo dependencies automatically installed:
 # libpng, freetype, fontconfig, pixman, gettext, libffi, glib
