fb9f1d2e930a71f83ff9cb7c54f93280392c4dd3
Felix <fdubrownik@gmail.com>
2015-04-28 12:31:04 +0200

    added opencl to homebrew

--- dev/install/osx/homebrew.sh
+++ dev/install/osx/homebrew.sh
@@ -2,7 +2,8 @@
 
 brew update
 brew install Caskroom/cask/xquartz # needed for cairo
-brew install libsndfile cairo pkgconfig cmake openssl
+brew install libsndfile cairo pkgconfig cmake openssl libusb
+brew install homebrew/science/opencv --c++11 --without-python --without-numpy --without-tests
 
 # cairo dependencies automatically installed:
 # libpng, freetype, fontconfig, pixman, gettext, libffi, glib
