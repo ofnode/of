b6e0415189ee0c158401e965588ffd4c5746b4f5
Constantine Tarasenkov <constantine@protonmail.com>
2015-07-09 14:56:13 +0300

    Change patch logic

--- dev/patch.sh
+++ dev/patch.sh
@@ -11,15 +11,24 @@ function apply() {
 
 #-------------------------------------------------------------------------------
 
-cd "$OF/src"
+cd "$OF/src/glfw"
 
-apply poco.patch
-apply videoinput.patch
-apply poco_cmake.patch
 apply glfw_cmake.patch
-apply openframeworks.patch
-apply openframeworks_modules.patch
 
 cd "$OF/src/libtess2"
 
 apply tess2.patch
+
+cd "$OF/src/poco"
+
+apply poco.patch
+apply poco_cmake.patch
+
+cd "$OF/src/videoinput"
+
+apply videoinput.patch
+
+cd "$OF/src/openframeworks"
+
+apply openframeworks.patch
+apply openframeworks_modules.patch
