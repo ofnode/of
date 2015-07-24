b4b46046c3452fba3c68beb382248dd42e7e5ce2
Constantine Tarasenkov <constantine@protonmail.com>
2015-07-24 22:17:20 +0300

    Continuing to fix issue #15

--- dev/clone.sh
+++ dev/clone.sh
@@ -18,7 +18,7 @@ mkdir -p src
 cd src
 
 # v 3.17.0 release
-setup freeimage 5870bda https://github.com/ofnode/freeimage
+setup freeimage 55c7e68 https://github.com/ofnode/freeimage
 
 # v 1.12.0 master
 setup glew 4c40805 https://github.com/nigels-com/glew
