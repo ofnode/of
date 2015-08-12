4ec5494fd591f274253b2f2f4322d5e66855bda9
Constantine Tarasenkov <constantine@protonmail.com>
2015-08-12 14:13:27 +0300

    Don't install Boost 1.55.0 on Ubuntu explicitly

--- dev/install/linux/ubuntu.sh
+++ dev/install/linux/ubuntu.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
-# Tested on Ubuntu 14.04 and 15.04
+# Tested on Ubuntu 14.04
 
 sudo apt-get update
 sudo apt-get -y upgrade
 
-sudo apt-get -y install     \
-git                         \
-gdb                         \
-pkg-config                  \
-xorg-dev                    \
-libgtk-3-dev                \
-libboost-filesystem1.55-dev \
-libboost-system1.55-dev     \
-libudev-dev                 \
-clang-3.6                   \
-cmake                       \
-ninja-build                 \
-libcairo2-dev               \
-libusb-1.0-0-dev            \
-libssl-dev                  \
-libfreetype6-dev            \
-libfontconfig1-dev          \
-libglu1-mesa-dev            \
-libmpg123-dev               \
-libopenal-dev               \
-libassimp-dev               \
-libopencv-dev               \
-libtbb-dev                  \
-libasound2-dev              \
-libsndfile1-dev             \
-gstreamer1.0-x              \
-gstreamer1.0-alsa           \
-gstreamer1.0-libav          \
-gstreamer1.0-pulseaudio     \
-gstreamer1.0-plugins-base   \
-gstreamer1.0-plugins-good   \
-gstreamer1.0-plugins-bad    \
-gstreamer1.0-plugins-ugly   \
-libgstreamer1.0-dev         \
+sudo apt-get -y install   \
+git                       \
+gdb                       \
+pkg-config                \
+xorg-dev                  \
+libgtk-3-dev              \
+libboost-filesystem-dev   \
+libboost-system-dev       \
+libudev-dev               \
+clang-3.6                 \
+cmake                     \
+ninja-build               \
+libcairo2-dev             \
+libusb-1.0-0-dev          \
+libssl-dev                \
+libfreetype6-dev          \
+libfontconfig1-dev        \
+libglu1-mesa-dev          \
+libmpg123-dev             \
+libopenal-dev             \
+libassimp-dev             \
+libopencv-dev             \
+libtbb-dev                \
+libasound2-dev            \
+libsndfile1-dev           \
+gstreamer1.0-x            \
+gstreamer1.0-alsa         \
+gstreamer1.0-libav        \
+gstreamer1.0-pulseaudio   \
+gstreamer1.0-plugins-base \
+gstreamer1.0-plugins-good \
+gstreamer1.0-plugins-bad  \
+gstreamer1.0-plugins-ugly \
+libgstreamer1.0-dev       \
 libgstreamer-plugins-base1.0-dev
 
 sudo rm -f /usr/bin/clang   && sudo ln -s /usr/bin/clang-3.6   /usr/bin/clang
