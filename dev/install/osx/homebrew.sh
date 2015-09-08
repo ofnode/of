ee35f4107632aa8f4637d8140b3e79bcca380575
Constantine Tarasenkov <constantine@protonmail.com>
2015-09-08 17:01:40 +0300

    Travis CI: don't print package installs to log

--- dev/install/osx/homebrew.sh
+++ dev/install/osx/homebrew.sh
@@ -3,12 +3,12 @@
 # Workaround for issue #15
 sudo cp /usr/include/assert.h /usr/local/include/
 
-brew update > /dev/null
+brew update
 
-brew install --quiet boost --c++11
-brew install --quiet Caskroom/cask/xquartz # Needed for Cairo
-brew install --quiet libsndfile cairo pkgconfig cmake ninja openssl libusb assimp mpg123
-brew install --quiet homebrew/science/opencv --c++11 --without-python --without-numpy --without-tests
+brew install boost --c++11
+brew install Caskroom/cask/xquartz # Needed for Cairo
+brew install libsndfile cairo pkgconfig cmake ninja openssl libusb assimp mpg123
+brew install homebrew/science/opencv --c++11 --without-python --without-numpy --without-tests
 
 # Cairo dependencies automatically installed:
 # libpng, freetype, fontconfig, pixman, gettext, libffi, glib
