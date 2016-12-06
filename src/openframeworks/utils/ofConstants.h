f8b80588d6b5ea62a449ae69b0b1d4681284fe59
Antoine Villeret <antoine.villeret@gmail.com>
2016-12-06 11:58:44 +0100

    sync with openFrameworks 0.9.8 https://github.com/openframeworks/openFrameworks/commit/8c7ddba5b7a8c7e58452ec3113da348d5d4bc693

--- src/openframeworks/utils/ofConstants.h
+++ src/openframeworks/utils/ofConstants.h
@@ -4,7 +4,7 @@
 //-------------------------------
 #define OF_VERSION_MAJOR 0
 #define OF_VERSION_MINOR 9
-#define OF_VERSION_PATCH 3
+#define OF_VERSION_PATCH 6
 #define OF_VERSION_PRE_RELEASE "stable"
 
 //-------------------------------
@@ -256,8 +256,11 @@ typedef TESSindex ofIndexType;
 		//on 10.6 and below we can use the old grabber
 		#ifndef MAC_OS_X_VERSION_10_7
 			#define OF_VIDEO_CAPTURE_QUICKTIME
-		#else
+		//if we are below 10.12 or targeting below 10.12 we use QTKit
+		#elif !defined(MAC_OS_X_VERSION_10_12) || MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_12
 			#define OF_VIDEO_CAPTURE_QTKIT
+		#else
+			#define OF_VIDEO_CAPTURE_AVF
         #endif
 
 	#elif defined (TARGET_WIN32)
@@ -357,7 +360,7 @@ typedef TESSindex ofIndexType;
 // on c++11, this is a workaround that bug
 #ifndef HAS_TLS
 	#if __clang__
-		#if __has_feature(cxx_thread_local) && !defined(__MINGW64__) && !defined(__MINGW32__)
+		#if __has_feature(cxx_thread_local) && !defined(__MINGW64__) && !defined(__MINGW32__) && !defined(TARGET_OF_IOS)
 			#define HAS_TLS 1
 		#endif
     #elif !defined(TARGET_WIN32) || _MSC_VER
