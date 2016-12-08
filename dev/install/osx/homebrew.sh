3b1fdc176fc3461f904f362365af34fea77d38d1
Antoine Villeret <antoine.villeret@gmail.com>
2016-12-08 14:45:07 +0100

    update OS X travis install script

--- dev/install/osx/homebrew.sh
+++ dev/install/osx/homebrew.sh
@@ -2,10 +2,11 @@
 
 brew update
 
+brew unlink boost
 brew install boost --c++11
 brew install Caskroom/cask/xquartz # Needed for Cairo
-brew install libsndfile cairo pkgconfig cmake ninja openssl libusb assimp mpg123 tbb
-brew install homebrew/science/opencv --c++11 --without-python --without-numpy --without-tests
+brew install libsndfile cairo ninja libusb assimp mpg123 tbb
+travis_wait 30 brew install homebrew/science/opencv --c++11 --without-python --without-numpy --without-test
 
 # Cairo dependencies automatically installed:
-# libpng, freetype, fontconfig, pixman, gettext, libffi, glib
+# libpng, freetype, fontconfig, pixman, gettext, libffi, glib, openssl, pkgconfig, cmake
