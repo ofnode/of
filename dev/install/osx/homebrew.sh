ac5942fe7427f828efc41817e852df7f42132892
Antoine Villeret <antoine.villeret@gmail.com>
2016-12-08 16:58:43 +0100

    try to make OS X wait

--- dev/install/osx/homebrew.sh
+++ dev/install/osx/homebrew.sh
@@ -5,10 +5,10 @@ set -v
 brew update
 
 brew unlink boost
-travis_wait 30 brew install boost --c++11
+brew install boost --c++11
 brew install Caskroom/cask/xquartz # Needed for Cairo
 brew install libsndfile cairo ninja libusb assimp mpg123 tbb
-travis_wait 30 brew install homebrew/science/opencv --c++11 --without-python --without-numpy --without-test
+brew install homebrew/science/opencv --c++11 --without-python --without-numpy --without-test
 
 # Cairo dependencies automatically installed:
 # libpng, freetype, fontconfig, pixman, gettext, libffi, glib, openssl, pkgconfig, cmake
