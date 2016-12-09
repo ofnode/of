82edac421dadfe41834ed702dd671b5bc2a71978
Antoine Villeret <antoine.villeret@gmail.com>
2016-12-09 10:08:12 +0100

    try to use already installed boost lib on Travis OS X

--- dev/install/osx/homebrew.sh
+++ dev/install/osx/homebrew.sh
@@ -4,8 +4,8 @@ set -v
 
 brew update
 
-brew unlink boost
-brew install boost --c++11
+# brew unlink boost
+# brew install boost --c++11
 brew install Caskroom/cask/xquartz # Needed for Cairo
 brew install libsndfile cairo ninja libusb assimp mpg123 tbb
 brew install homebrew/science/opencv --c++11 --without-python --without-numpy --without-test
