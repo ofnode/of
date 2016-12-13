ed895365cfb8cad1d54ac96da91d121e2c2b038e
Antoine Villeret <antoine.villeret@gmail.com>
2016-12-13 22:14:33 +0100

    fix path

--- travis/deps.sh
+++ travis/deps.sh
@@ -2,14 +2,14 @@
 
 case "$TRAVIS_OS_NAME" in
   linux)
-    $TRAVIS_BUILD_DIR/of/dev/install/linux/ubuntu.sh
+    $TRAVIS_BUILD_DIR/dev/install/linux/ubuntu.sh
 
     wget https://cmake.org/files/v3.7/cmake-3.7.0-Linux-x86_64.tar.gz -O cmake-linux.tgz
     tar xaf cmake-linux.tgz
     mv cmake-*-x86_64 cmake
   ;;
   osx)
-    $TRAVIS_BUILD_DIR/of/dev/install/osx/homebrew.sh
+    $TRAVIS_BUILD_DIR/dev/install/osx/homebrew.sh
   ;;
 esac
 
