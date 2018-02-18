c7f46cef8c5179ccff39e08fe713d3d8b24fd385
Tempo <tempo@tempo.home>
2018-02-19 00:48:25 +0100

    remote deprecated formula

--- dev/install/osx/homebrew.sh
+++ dev/install/osx/homebrew.sh
@@ -8,7 +8,7 @@ brew update
 # brew install boost --c++11
 brew install Caskroom/cask/xquartz # Needed for Cairo
 brew install libsndfile cairo ninja libusb assimp mpg123 tbb
-brew install homebrew/science/opencv --c++11 --without-python --without-numpy --without-test
+brew install opencv@2 --c++11 --without-python --without-numpy --without-test
 
 # Cairo dependencies automatically installed:
 # libpng, freetype, fontconfig, pixman, gettext, libffi, glib, openssl, pkgconfig, cmake
